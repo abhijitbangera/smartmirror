59b9df584612cc79d0038419b9c4cacfb045c049
Abhijit <abhijit.bangera@hotmail.com>
2016-03-22 23:40:36 +0530

    prediction and top news feature added

--- mirror/mirror/views.py
+++ mirror/mirror/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from datetime import timedelta,datetime,date,time
 import pywapi
+from lxml import html
+import requests
+
 def mypage(request):
 	weather_com_result = pywapi.get_weather_from_weather_com('INXX0012')
 	date1=date.today()
@@ -10,12 +13,7 @@ def mypage(request):
 	Location="Bangalore"
 	humidity=weather_com_result['current_conditions']['humidity']
 	result = pywapi.get_weather_from_yahoo('INXX0012', 'metric') 
-	print(result)
-	print("**************************")
-	print(weather_com_result["forecasts"][1]["day"]["text"])
-
-
-	print("***************")
+	
 	day_plus1=weather_com_result["forecasts"][1]["day_of_week"]
 	day_plus1_type=weather_com_result["forecasts"][1]["day"]["text"]
 	day_plus1_low=weather_com_result["forecasts"][1]["low"]
@@ -37,7 +35,20 @@ def mypage(request):
 	day_plus4_low=weather_com_result["forecasts"][4]["low"]
 	day_plus4_high=weather_com_result["forecasts"][4]["high"]
 
-	print(day_plus1)
+	page = requests.get('http://www.ganeshaspeaks.com/leo/leo-daily-horoscope.action')
+	html1 = html.fromstring(page.content)
+	prediction = html1.xpath('//*[@id="main-wrapper"]/div/div/div[2]/section/div[2]/div/div/div[1]/span/text()')[0].replace('Ganesha','Magic Mirror')
+
+	newspage = requests.get('https://news.google.co.in/')
+	html2 = html.fromstring(newspage.content)
+	news1 = html2.xpath('//*[@id="MAA4AEgAUABgAWoCaW4"]/span/text()')[0]
+	news2 = html2.xpath('//*[@id="MAA4AEgBUABgAWoCaW4"]/span/text()')[0]
+	news3 = html2.xpath('//*[@id="MAA4AEgCUABgAWoCaW4"]/span/text()')[0]
+	news4 = html2.xpath('//*[@id="MAA4AEgDUABgAWoCaW4"]/span/text()')[0]
+	news5 = html2.xpath('//*[@id="MAA4AEgEUABgAWoCaW4"]/span/text()')[0]
+
+
+	print(news1)
 	context={'name':'Abhi',
 			 'date': date1,
 			 'time':time1,
@@ -63,9 +74,16 @@ def mypage(request):
 			 'day_plus4_low':day_plus4_low,
 			 'day_plus4_high':day_plus4_high,
 
+			 'prediction': prediction,
+
+			 'news1':news1,
+			 'news2':news2,
+			 'news3':news3,
+			 'news4':news4,
+			 'news5':news5,
+			 
 			 }
 	
  	
-	print ("Weather.com says: It is " + weather_com_result['current_conditions']['text'].lower() + " and " + weather_com_result['current_conditions']['temperature'] + "°C now in London.")
-
+	
 	return render(request, "index.html", context)
