16fdce7f241c7f9fc190d02a72aeeacd40818268
Abhijit <abhijit.bangera@hotmail.com>
2016-03-24 23:05:04 +0530

    Google Calender code integrated

--- mirror/mirror/views.py
+++ mirror/mirror/views.py
@@ -4,6 +4,45 @@ import pywapi
 from lxml import html
 import requests
 
+#Google calendar app dependents
+import httplib2
+import os
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    print(home_dir)
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
 def mypage(request):
 	weather_com_result = pywapi.get_weather_from_weather_com('INXX0012')
 	date1=date.today()
@@ -48,6 +87,47 @@ def mypage(request):
 	news5 = html2.xpath('//*[@id="MAA4AEgEUABgAWoCaW4"]/span/text()')[0]
 
 
+	#Google calendar app code
+	try:
+	    import argparse
+	    from argparse import Namespace
+	    flags=Namespace(auth_host_name='localhost', auth_host_port=[8080, 8090], logging_level='ERROR', noauth_local_webserver=False)
+	except ImportError:
+	    flags = None
+
+	print(flags)
+	# If modifying these scopes, delete your previously saved credentials
+	# at ~/.credentials/calendar-python-quickstart.json
+	SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+	CLIENT_SECRET_FILE = 'client_secret.json'
+	APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+
+	credentials = get_credentials()
+	http = credentials.authorize(httplib2.Http())
+	service = discovery.build('calendar', 'v3', http=http)
+	now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+	print('Getting the upcoming 10 events')
+	eventsResult = service.events().list(
+		calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+		orderBy='startTime').execute()
+	events = eventsResult.get('items', [])
+	cal_events=[]
+	if not events:
+		print('No upcoming events found.')
+		cal_events="No upcoming events found."
+	print(events)
+	for event in events:
+		start = event['start'].get('dateTime', event['start'].get('date'))
+		cal_events.append("Title:"+ event['summary'] + " " + "Time:" +start[12:19]+ " " +"Date:" +start[0:10] )	
+	# print(start, event['summary'])
+	print(cal_events)
+
+
+    ##########################################################
+
+
 	print(news1)
 	context={'name':'Abhi',
 			 'date': date1,
@@ -81,6 +161,8 @@ def mypage(request):
 			 'news3':news3,
 			 'news4':news4,
 			 'news5':news5,
+
+			 'cal_events':cal_events,
 			 
 			 }
 	
